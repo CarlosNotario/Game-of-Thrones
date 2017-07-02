17bf7dd4eec8cc05aef2c9b2ff49eafe66091a0e
Carlos Notario <carlosnotario30@gmail.com>
2017-07-02 13:40:34 -0400

    Updated controller and view.

--- Gameofthrones/Controllers/GOTcontroller.cs
+++ Gameofthrones/Controllers/GOTcontroller.cs
@@ -8,12 +8,23 @@ namespace Gameofthrones.Controllers
 {
     public class GOTcontroller : Controller
     {
-        public ActionResult Detail
+        public ActionResult Detail()
         {
-            get
+            ViewBag.SeriesTitle = "Winter Is Coming";
+            ViewBag.EpisodeNumber = 0001;
+            ViewBag.Description = "<p>Jon Arryn, the Hand of the King, is dead. King Robert Baratheon plans to ask his oldest friend, Eddard Stark, to take Jon's place. Across the sea, Viserys Targaryen plans to wed his sister to a nomadic warlord in exchange for an army.</p>";
+            ViewBag.Credits = new string[]
             {
-                return View();
-            }
+                    "Directed by Timothy Van Patten  ... (as Tim Van Patten)",
+                    "Writing Credits, " +
+                    " David Benioff   ... (created by)",
+                    "D.B. Weiss  ... (created by)",
+                    "George R.R. Martin  ... (based on 'A Song of Ice and Fire' by)",
+                    "David Benioff   ... (written by)",
+                    "D.B. Weiss  ... (written by)"
+            };
+                          
+            return View();
         }
     }
 }
